d17561dda568745de6df5cad48334fc1ab74061a
DrDesten <>
2022-04-22 17:00:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Automatic Semicolon Insertion for GLSL
+
+## Features
+
+This formatter automatically adds semicolons to GLSL files
---
+++ extension.js
@@ -0,0 +1,67 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require( 'vscode' )
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate( context ) {
+
+	console.log( '"asi-glsl" is online' )
+
+	let disposable = vscode.languages.registerDocumentFormattingEditProvider( "glsl", {
+		provideDocumentFormattingEdits( document ) {
+
+			// ASI | Semicolon after structs
+			//const asi = /((?<!(struct|void|uniform|in|out|varying|(i|u)?(sampler|image)([123]D|Cube|2DRect|[12]DArray|CubeArray|Buffer|2DMS|2DMSArray)(Shadow)?|bool|u?int|float|half|double|(b|i|u|d)?vec[2-4]|d?mat[2-4](x[2-4])?|[{}(\].=?+\-*/%<>!&^|,;\n])\s*?)\n(?=[ \t]*[^ .=?+\-*/%<>!&^|,])|(?<=struct\s+\w+\s+{[^{}]+?})\s(?!\s*;))/g
+			const asi = /((?<!(struct|void|uniform|in|out|varying|(i|u)?(sampler|image)([123]D|Cube|2DRect|[12]DArray|CubeArray|Buffer|2DMS|2DMSArray)(Shadow)?|bool|u?int|float|half|double|(b|i|u|d)?vec[2-4]|d?mat[2-4](x[2-4])?|[{}(\].=?+\-*/%<>!&^|,;\s]))(?=\s*?\n\s*?[^ .=?+\-*/%<>!&^|,({])|(?<=struct\s+\w+\s+{[^{}]+?})\s(?![\t\f\v \u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*[;\w]))/g
+			const ignore = /\/\/.*|\/\*[^]*?\*\/|#.*/g
+
+			// Replace all areas that should not be parsed with whitspace so that ASI won't match it
+			/////////////////////////////////////////////////////////////////////////////////////////////
+			let searchString = document.getText().replace( ignore, function ( substr ) { return " ".repeat( substr.length ) } ) // Replace Comments and Preprocessor Directives
+			for ( let i = 0, priority = 0; i < searchString.length; i++ ) {                                 // Replace Content of Parentheses and Brackets '()' and '[]'
+				let char = searchString.slice( i, i + 1 )
+				if ( char == ")" || char == "]" ) priority--
+				if ( priority > 0 ) searchString = searchString.slice( 0, i ) + " " + searchString.slice( i + 1 )
+				if ( char == "(" || char == "[" ) priority++
+			}
+			searchString += "\na"
+
+			// Insert Semicolons at the correct locations
+			/*////////////////////////////////////////////////////////////////////////////////////////////
+			Here I hijack the str.replace function:
+			The function I pass in doesn't actually change the string, but modifies the original string. 
+			arguments[3] corresponds to the index, which matches because I replaced all unparsed code with whitespace and didn't delete it.
+			Since the string length increases with each insert, I keep track of this using replaceCount.
+			The semicolon has to be inserted after the first match group, arguments[1], so I add its length to the index
+			*/
+			let edits = []
+			let replaceCount = 0
+			searchString.replace( asi, function () {
+				let index = arguments[ arguments.length - 2 ]
+				if ( index <= 1 ) return
+				//console.log( index )
+				edits.push( vscode.TextEdit.insert( document.positionAt( index ), ";" ) )
+				replaceCount++
+			} )
+
+			if ( edits.length > 0 ) vscode.window.showInformationMessage( `Added ${edits.length} Semicolon${"s".repeat( edits.length != 1 )}` )
+
+			return edits
+		}
+	} )
+
+	context.subscriptions.push( disposable )
+}
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
